ef647c4bd3a8762a348f7d207e8c5e601d5e8ad0
Vishal Rana <vishal2.rana@airtel.com>
2023-06-28 20:35:54 +0530

    Refactoring

--- src/main/java/com/vishal/enums/SpotStrategyType.java
+++
@@ -1,7 +0,0 @@
-package com.vishal.enums;
-
-public enum SpotStrategyType {
-    NEAREST_TO_ELEVATOR_STRATEGY,
-    NEAREST_TO_ENTRY_GATE_STRATEGY,
-    LINEAR_STRATEGY
-}
--- src/main/java/com/vishal/model/strategy/FairCalculationStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.vishal.model.strategy;
-
-import com.vishal.model.Ticket;
-
-public interface FairCalculationStrategy {
-    double calculateFair(Ticket ticket);
-}
